5ea295b9d060034d0de24f9359355329ef2cd201
Morris van den Bergh <morrisvandenbergh@gmail.com>
2025-08-29 19:34:41 +0200

    fixed requirements and the dockerfile

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,21 +3,19 @@ from discord.ext import commands
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from config import database_uri
+from config import database_uri, TOKEN
 
 intents = discord.Intents.all()
 bot = discord.Bot(intents=intents, prefix="!")
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
 
 for i in range(5):
     try:
         engine = create_engine(database_uri)
-        # engine = create_engine('sqlite:///database.db')
+        
         Base = declarative_base()
         Session = sessionmaker(bind=engine)
         db = Session()
+        Base.metadata.create_all(engine)
         break
         
     except Exception as e:
--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 db_username = os.getenv('DB_USERNAME')
 db_password = os.getenv('DB_PASSWORD')
@@ -6,4 +11,4 @@ db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 database_uri = "sqlite:///database.db"
-# database_uri = f"sqlite:///{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+# database_uri = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
